8becfb8a88d8d0efd0c71113153e7bd4de397f6f
jonas111996 <124303273+jonas111996@users.noreply.github.com>
2023-05-22 13:20:09 +0200

    Update SML kode.R

--- SML kode.R
+++ SML kode.R
@@ -20,7 +20,7 @@ market_data <- tq_get(market_ticker,
                       to = '2022-05-31',
                       get = 'stock.prices')
 
-# Calculate log returns
+# Calculate daily log returns
 log_ret_tidy_assets <- asset_data %>%
   group_by(symbol) %>%
   tq_transmute(select = adjusted,
@@ -37,7 +37,7 @@ log_ret_tidy_market <- market_data %>%
                type = 'log') %>%
   mutate(symbol = market_ticker)  # Add the symbol column manually
 
-# Convert to xts format
+# Convert to xts format, was in tidy format before
 log_ret_xts_assets <- log_ret_tidy_assets %>%
   spread(symbol, value = ret) %>%
   tk_xts()
@@ -46,7 +46,7 @@ log_ret_xts_market <- log_ret_tidy_market %>%
   spread(symbol, value = ret) %>%
   tk_xts()
 
-# Calculate the annualized returns
+# Calculate the annualized returns (Colmeans calculate the mean - 252 financial days in a year)
 ann_ret <- colMeans(log_ret_xts_assets) * 252
 
 # Calculate the beta for each asset
@@ -56,7 +56,7 @@ beta_values <- sapply(1:ncol(log_ret_xts_assets), function(i) {
 
 names(beta_values) <- colnames(log_ret_xts_assets) # name the beta values
 
-# Add risk free rate
+# Add risk-free rate
 rf <- 0.02
 
 # Calculate the expected market return
@@ -69,15 +69,15 @@ asset_df <- data.frame(
   Return = ann_ret
 )
 
-# Calculate the SML
+# Calculate the Security Market Line (SML) function
 sml <- function(beta) rf + beta * (E_Rm - rf)
 
 
 # Create the plot
 ggplot(asset_df, aes(x = Beta, y = Return)) +
   geom_point() +
-  geom_text(aes(label = Ticker), vjust = -1, hjust = -0.5) +  # Add this line
-  stat_function(fun = sml, color = "red", linetype = "dashed") +
+  geom_text(aes(label = Ticker), vjust = -1, hjust = -0.5) +  # Add labels to data points
+  stat_function(fun = sml, color = "red", linetype = "dashed") +  # Add SML line
   labs(x = "Beta", y = "Annualized Returns") +
   xlim(-1, 2) +  
   ylim(-0.2, 0.4) +
